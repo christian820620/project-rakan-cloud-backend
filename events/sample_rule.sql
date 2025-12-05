06f53e478bec72529e78c86b85a9383be5dfdeb8
Vink3r <101687649+Vink3r@users.noreply.github.com>
2025-12-04 19:03:01 -0500

    Create sample_rule.sql

---
+++ events/sample_rule.sql
@@ -0,0 +1,3 @@
+SELECT ID, humidity, temperature, temperature, fan_speed_percent, heat, ac FROM 'thermostat/pub' WHERE ID = 1
+
+--Example how we setup SQL rule to invoke Lambda function to log our IoT Publish data
